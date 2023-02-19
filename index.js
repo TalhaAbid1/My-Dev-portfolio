67fb1d90965f557a47eaade56233bf64509f2ca5
Talha Abid <192400002@gift.edu.pk>
2023-02-19 15:11:56 +0500

    Responsive Analogue Clock Added

--- index.js
+++ index.js
@@ -98,3 +98,20 @@ function Hamburger(){
   document.getElementById("Cross").style.webkitTransform = 'rotate(0deg)';
   document.getElementById("SlidingMenu").style.right = "0";
 }
+
+// RealTime Avalogue Clock 
+setInterval(()=>{
+	d = new Date();
+	htime=d.getHours();
+	mtime=d.getMinutes();
+	stime=d.getSeconds();
+
+	hRotation = 30*htime + mtime/2;
+	mRotation = 6*mtime;
+	sRotation = 6*stime;
+
+	hours.style.transform = `rotate(${hRotation}deg)`;
+	minutes.style.transform = `rotate(${mRotation}deg)`;
+	seconds.style.transform = `rotate(${sRotation}deg)`;
+
+},1000);
